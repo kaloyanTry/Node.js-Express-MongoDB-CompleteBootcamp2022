6d89ff0b849ded0fb7b09e96811aa48408527c13
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2022-01-03 20:45:58 +0200

    Initial code

---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const http = require("http");
+
+// const hello = "Hello world";
+// console.log(hello);
+////////////////////////////////////////////////////////////
+// Blocking, syncronous way - Non-good practice:
+// Read stuff from files:
+// const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textInput);
+
+// // Write text on file:
+// const textOutput = `This is all we know about the avocado: ${textInput}.\nCreated on ${Date.now()}.`;
+
+// fs.writeFileSync("./txt/output.txt", textOutput);
+// console.log("File written!");
+
+// Non-blocking, asynchronous way: Good practice:
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("Error 💣");
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has not been written 🤕");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read file.");
+/////////////////////////////////////////////////////////////////////
+// SERVER
+
+const server = http.createServer((req, res) => {
+  // console.log(req);
+  res.end("Hello from the server!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
