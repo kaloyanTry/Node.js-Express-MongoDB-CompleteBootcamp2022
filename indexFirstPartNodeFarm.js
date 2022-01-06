00291f906a17414c66cb3894da8d9638b5774587
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2022-01-06 21:21:34 +0200

    Add files via upload

--- indexFirstPartNodeFarm.js
+++ indexFirstPartNodeFarm.js
@@ -66,10 +66,9 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const { query, pathname } = url.parse(req.url, true);
-
+  const pathName = req.url;
   // Overview page
-  if (pathname === "/" || pathname === "/overview") {
+  if (pathName === "/" || pathName === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHtml = dataObj
@@ -79,11 +78,9 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     // Product page
-  } else if (pathname === "/product") {
+  } else if (pathName === "/product") {
     res.writeHead(200, { "Content-type": "text/html" });
-    const product = dataObj[query.id];
-    const output = replaceTemplate(tempProduct, product);
-    res.end(output);
+    res.end(tempProduct);
 
     // API
   } else if (pathName === "/api") {
