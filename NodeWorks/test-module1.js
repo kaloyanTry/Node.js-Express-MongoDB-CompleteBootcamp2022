7fc068e776a3987e79418503bcbef00282e8b140
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2022-01-10 21:36:29 +0200

    Add files via upload

---
+++ NodeWorks/test-module1.js
@@ -0,0 +1,26 @@
+class Calculator {
+  add(a, b) {
+    return a + b;
+  }
+  multiply(a, b) {
+    return a * b;
+  }
+  divide(a, b) {
+    return a / b;
+  }
+}
+
+module.exports = Calculator;
+
+// The same functionality, but directly:
+// module.exports = {
+//   add(a, b) {
+//     return a + b;
+//   }
+//   multiply(a, b) {
+//     return a * b;
+//   }
+//   divide(a, b) {
+//     return a / b;
+//   }
+// }
