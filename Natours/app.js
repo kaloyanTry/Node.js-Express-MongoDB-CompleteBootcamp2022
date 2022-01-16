e6d58ccaadf62eebac169323e19a0f33a11e8da8
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2022-01-16 08:02:40 +0200

    Add files via upload

--- Natours/app.js
+++ Natours/app.js
@@ -5,10 +5,12 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from middleware.');
