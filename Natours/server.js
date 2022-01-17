1def2e6bfff94d540820b3f69e00b2c7599f832b
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2022-01-17 21:17:55 +0200

    Add files via upload

--- Natours/server.js
+++ Natours/server.js
@@ -1,7 +1,8 @@
+/* eslint-disable prettier/prettier */
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
