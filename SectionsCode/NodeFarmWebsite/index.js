687e8fa4456a406c49cbb7041f30602e7012e9c2
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2022-05-26 21:04:52 +0300

    Add files via upload

--- SectionsCode/NodeFarmWebsite/index.js
+++ SectionsCode/NodeFarmWebsite/index.js
@@ -2,8 +2,23 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-// Creating the server:
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf8');
+// Creating replacing function: template to current product:
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+  return output;
+};
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf8');
 
@@ -11,19 +26,27 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  //const pathName = req.url;
+  // distructuring variables: query & pathname:
+  const { query, pathname } = url.parse(req.url, true);
 
   //Page overview:
-  if (pathName === '/' || pathName === '/overview') {
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' });
-    res.end(tempOverview);
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
 
     //Page product:
-  } else if (pathName === 'product') {
-    res.end('PRODUCT');
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const product = dataObj[query.id]; //Retriving the product
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     //API:
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
 
