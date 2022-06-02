13ce42e6a5127e22732268e98d583d1f9316ec2c
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2022-06-02 07:47:18 +0300

    clear code
    Cleared deconstructions code

--- SectionsCode/section-4/module.js
+++ SectionsCode/section-4/module.js
@@ -7,7 +7,7 @@
 // using properties directly with exports:
 // const calc = require("./module-export");
 // console.log(calc.add(2, 5));
-
 // or deconstruction variables:
 const { add, multiply } = require("./module-export");
 console.log(add(3, 7));
+console.log(multiply(3, 7));
