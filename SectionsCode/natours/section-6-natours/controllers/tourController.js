318cc2cdf34d70f6e722507fc48963e1565c77eb
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2022-06-10 17:40:31 +0300

    Update tourController.js

--- SectionsCode/natours/section-6-natours/controllers/tourController.js
+++ SectionsCode/natours/section-6-natours/controllers/tourController.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
 exports.checkID = (req, res, next, val) => {
   console.log(`Tour id is: ${val}`);
