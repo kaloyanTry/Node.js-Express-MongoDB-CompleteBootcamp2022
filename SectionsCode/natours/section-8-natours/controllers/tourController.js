54e31f965af45f023c6c1d9c62da0a6190e67e7c
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2022-06-13 21:20:54 +0300

    Added filtering and sorting
    Added filtering and sorting functionality.

--- SectionsCode/natours/section-8-natours/controllers/tourController.js
+++ SectionsCode/natours/section-8-natours/controllers/tourController.js
@@ -2,13 +2,29 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    // Build query:
+    // 1.1.) Filtering:
     const queryObj = { ...req.query };
     const exludedFields = ['page', 'sort', 'limit', 'fields'];
     exludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    //const query = Tour.find(queryObj);
 
+    // 1.2.) Advanced filtering:
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    let query = Tour.find(JSON.parse(queryStr));
     const tours = await query;
+
+    // 2. Sorting:
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     res.status(200).json({
       status: 'success',
