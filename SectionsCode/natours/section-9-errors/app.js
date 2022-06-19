4b562cf36acd8a4d982ae8429315459e7e848fde
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2022-06-19 21:49:22 +0300

    Add files via upload

--- SectionsCode/natours/section-9-errors/app.js
+++ SectionsCode/natours/section-9-errors/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -23,10 +25,10 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server.`,
-  });
+  next(new AppError(`Can't find ${req.originalUrl} on this server`));
 });
+
+// Error handling:
+app.use(globalErrorHandler);
 
 module.exports = app;
