d87d816d41510c649a01af94869f0fbd79561202
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2022-07-04 13:57:06 +0300

    Add files via upload

--- SectionsCode/natours/section-10-auth/controllers/authController.js
+++ SectionsCode/natours/section-10-auth/controllers/authController.js
@@ -172,3 +172,18 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1) Get user from the collection
+  const user = await User.findById(req.user.id).select('+password');
+
+  // 2) Check if POSTed current password is correct:
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is incorrect', 401));
+  }
+
+  // 3) if so, update user:
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+});
