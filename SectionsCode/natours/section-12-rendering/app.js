47a069bb4dc6139562c4b73bcb430f0aaa9309e6
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2022-07-27 19:22:35 +0300

    Add files via upload

--- SectionsCode/natours/section-12-rendering/app.js
+++ SectionsCode/natours/section-12-rendering/app.js
@@ -6,7 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
-//const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -43,7 +43,7 @@ app.use('/api', limiter);
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
-//app.use(cookieParser());
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
